971658f192b3e72af43a8b45e37b8fef0b49126a
Pedro Ferraresi <ferraresi.pedro@gmail.com>
2022-11-22 17:17:54 -0300

    Finished V1

---
+++ main.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+import joblib
+import pandas as pd
+
+
+def read_file(file):
+    return pd.read_csv(file, delimiter=";")
+
+
+def load_pipeline(pipeline):
+    return joblib.load(pipeline)
+
+
+def predict_churn(dataframe, pipeline):
+    dataframe["Exited"] = pipeline.predict(dataframe)
+
+    return dataframe
+
+
+def export_predictions(dataframe):
+    date = datetime.now().date().strftime("%Y%m%d")
+
+    dataframe.loc[:, ["RowNumber", "Exited"]].to_csv(
+        f"./data/answer/abandonos_{date}.csv", index=False
+    )
+
+    return None
+
+
+def main():
+    # Load CSV File
+    df = read_file(f"./data/test/abandono_teste.csv")
+
+    # Load Pipeline
+    pipeline = load_pipeline(f"./models/pipeline.pkl")
+
+    # Predict Churns
+    df = predict_churn(df, pipeline)
+
+    # Export Answers
+    export_predictions(df)
+
+
+if __name__ == "__main__":
+    main()
